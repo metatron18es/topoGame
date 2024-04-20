4ae049030600a44b87c7e06b3b62d9410e8b7b65
Metatron <metatron18es@gmail.com>
2024-04-20 21:05:16 +0200

    feat: favicon

--- vite.config.ts
+++ vite.config.ts
@@ -21,13 +21,18 @@ export default defineConfig({
         theme_color: '#AFE57F',
         icons: [
           {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
             type: 'image/png',
           },
           {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
             type: 'image/png',
           },
         ],
