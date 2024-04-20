0f9d33b1091d40b29aec59daec95f50088c762ac
Metatron <metatron18es@gmail.com>
2024-04-20 20:53:37 +0200

    fix: Change tests

---
+++ src/components/__tests__/TopoMain.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import TopoMain from '../TopoMain.vue'
+
+describe('TopoMain', () => {
+  it('renders properly', () => {
+    const wrapper = mount(TopoMain)
+  })
+})
